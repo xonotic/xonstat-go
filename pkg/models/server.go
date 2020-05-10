bddcdf00a52169465e1b792269e3e23c23886682
Ant Zucaro <azucaro@gmail.com>
2020-05-10 19:44:45 -0400

    Add implementation of ServerByHashkey().

---
+++ pkg/models/server.go
@@ -0,0 +1,26 @@
+package models
+
+// ServerByHashkey retrives a server record by the server's hashkey value
+func (ds *PGDatastore) ServerByHashkey(hashkey string) (*Server, error) {
+	sql := `select server_id, name, location, ip_addr, port, hashkey, public_key, revision, 
+	pure_ind, impure_cvars, elo_ind, active_ind, create_dt
+	from servers
+	where hashkey = $1`
+
+	rows, err := ds.db.Query(sql, hashkey)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var s Server
+	for rows.Next() {
+		err := rows.Scan(&s.ServerId, &s.Name, &s.Location, &s.IPAddr, &s.Port, &s.HashKey, &s.PublicKey,
+			&s.Revision, &s.PureInd, &s.ImpureCvars, &s.EloInd, &s.ActiveInd, &s.CreateDt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &s, nil
+}
