6a42b0f5d3fba8d1c8329541583be7e1be1e79ef
Ant Zucaro <azucaro@gmail.com>
2020-05-10 18:58:02 -0400

    On second thought, put this in datastore and behind an interface.

---
+++ pkg/models/datastore.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+// IdleConnections is the maximum number of idle connections we should maintain for the database.
+const IdleConnections = 5
+
+// Datastore is the interface representing all database operations.
+// This is useful for mocking or implementing different backends.
+type Datastore interface {
+	ServerByHashkey(hashkey string) (*Server, error)
+}
+
+// PGDatastore is an implementation of the Datastore interface for a Postgresql database.
+type PGDatastore struct {
+	db *sql.DB
+}
+
+// NewPGDatastore creates a new concrete implementation of a Datastore.
+func NewPGDatastore(dsn string) (*PGDatastore, error) {
+	// establish a database connection
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	// connection pooling
+	db.SetMaxIdleConns(IdleConnections)
+
+	return &PGDatastore{db}, nil
+}
