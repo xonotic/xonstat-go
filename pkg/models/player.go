e4535f78fc76dcc6a06d63e54148c325abc56bd3
Ant Zucaro <azucaro@gmail.com>
2020-05-18 12:01:47 -0400

    Start Player datastore methods.

---
+++ pkg/models/player.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"fmt"
+	"strings"
+)
+
+// RPlayersByHashkeyMulti finds multiple players by their hashkeys, returning back a map
+// indexed by the hashkey with player record pointers as values.
+func (ds *PGDatastore) RPlayersByHashkeyMulti(hashkeys []string) (map[string]*Player, error) {
+	hashKeyInStr := strings.Join(hashkeys, ", ")
+
+	sql := fmt.Sprintf(`select p.player_id, p.nick, p.stripped_nick, p.location, p.email_addr, 
+	p.active_ind, p.create_dt, ph.hashkey
+	from players p join player_hashkeys ph on p.player_id = ph.player_id
+	where ph.hashkey in (%s)`, hashKeyInStr)
+
+	rows, err := ds.db.Query(sql)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var players map[string]*Player
+	for rows.Next() {
+		var p Player
+		var hashkey string
+
+		err := rows.Scan(&p.PlayerID, &p.Nick, &p.StrippedNick, &p.Location, &p.EmailAddr, &p.ActiveInd, 
+			&p.CreateDt, &hashkey)
+
+		if err != nil {
+			return nil, err
+		}
+
+		players[hashkey] = &p
+	}
+
+	return players, nil
+}
