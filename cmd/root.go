846c132a52a3891d96649e7d3f053e7b7943b053
Ant Zucaro <azucaro@gmail.com>
2020-06-06 20:14:43 -0400

    Remove dependency on homedir.

--- cmd/root.go
+++ cmd/root.go
@@ -5,8 +5,6 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 )
 
@@ -34,15 +32,8 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
 		// Search config in home directory with name ".xonstat" (without extension).
-		viper.AddConfigPath(home)
+		viper.AddConfigPath("$HOME")
 		viper.SetConfigName(".xonstat")
 	}
 
