052fe26675c71383b9aad9260d807b8c583babb9
Ant Zucaro <azucaro@gmail.com>
2020-08-16 16:34:53 -0400

    Add initial server_info page with header info.

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -35,5 +35,27 @@ func (ae *AppEnv) ServerInfoHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write(s)
 	} else {
 		// HTML response
+		s, err := server.ServerInfoData(ae.db, ID)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			http.Error(w, fmt.Sprintf("404 %s", http.StatusText(404)), 404)
+			return
+		}
+
+		// The structure passed to the template.
+		type Data struct {
+			Server server.ServerInfoBase
+		}
+
+		data := Data{
+			Server:  *s,
+		}
+
+		err = ae.templates["serverinfo.page.html"].Execute(w, data)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			http.Error(w, fmt.Sprintf("500 %s", http.StatusText(500)), 500)
+			return
+		}
 	}
 }
