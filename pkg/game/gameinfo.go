c1200a5b26aba0f07ae55b57b80424564202934e
Ant Zucaro <azucaro@gmail.com>
2020-08-28 09:43:58 -0400

    Display team game stats.

--- pkg/game/gameinfo.go
+++ pkg/game/gameinfo.go
@@ -1,9 +1,66 @@
 package game
 
 import (
+	"strings"
+
 	"gitlab.com/xonotic/xonstat/pkg/models"
 	"gitlab.com/xonotic/xonstat/pkg/server"
 )
+
+// TeamGameStatBase is the view agnostic representation of a team's stats
+type TeamGameStatBase struct {
+	TeamGameStatID int
+	GameID         int
+	Team           int
+	Score          int
+	Rounds         int
+	Caps           int
+	Color          string
+	ColorInitCap   string
+}
+
+// NewTeamGameStatBase creates an instance of this class from the model type 
+// returned from the DB.
+func NewTeamGameStatBase(tgs *models.TeamGameStat) *TeamGameStatBase {
+	score := 0
+	if tgs.Score.Valid {
+		score = int(tgs.Score.Int32)
+	}
+
+	rounds := 0
+	if tgs.Rounds.Valid {
+		rounds = int(tgs.Rounds.Int32)
+	}
+
+	caps := 0
+	if tgs.Caps.Valid {
+		caps = int(tgs.Caps.Int32)
+	}
+
+	color := "red"
+	if tgs.Team == 5 {
+		color = "red"
+	} else if tgs.Team == 14 {
+		color = "blue"
+	} else if tgs.Team == 13 {
+		color = "yellow"
+	} else if tgs.Team == 10 {
+		color = "pink"
+	}
+
+	colorInitCap := strings.Title(color)
+
+	return &TeamGameStatBase{
+		TeamGameStatID: tgs.TeamGameStatID,
+		GameID: tgs.GameID,
+		Team: tgs.Team,
+		Score: score,
+		Rounds: rounds,
+		Caps: caps,
+		Color: color,
+		ColorInitCap: colorInitCap,
+	}
+}
 
 // InfoBase is the view-agnostic representation of a Game.
 type InfoBase struct {
@@ -16,7 +73,7 @@ type InfoBase struct {
 	Mod               string
 	CreateDt          *models.MultiDt
 	Server            *server.InfoBase
-	TeamGameStats     []*models.TeamGameStat
+	TeamGameStats     []*TeamGameStatBase
 	PlayerGameStats   []*models.PlayerGameStat
 	PlayerWeaponStats []*models.PlayerWeaponStat
 }
@@ -38,9 +95,14 @@ func InfoData(db models.Datastore, gameID int) (*InfoBase, error) {
 		return nil, err
 	}
 
-	teamGameStats, err := db.RTeamGameStatsByGameID(gameID)
+	rawTeamGameStats, err := db.RTeamGameStatsByGameID(gameID)
 	if err != nil {
 		return nil, err
+	}
+
+	var teamGameStats []*TeamGameStatBase
+	for _, v := range rawTeamGameStats {
+		teamGameStats = append(teamGameStats, NewTeamGameStatBase(v))
 	}
 
 	playerGameStats, err := db.RPlayerGameStatsByGameID(gameID)
