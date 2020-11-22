e0518335da8ba142c0b08e7c88fc8950f945215d
Ant Zucaro <azucaro@gmail.com>
2020-11-22 09:54:36 -0500

    Shape the weapon data for NVD3.

--- internal/handlers/game.go
+++ internal/handlers/game.go
@@ -126,9 +126,6 @@ func (ae *AppEnv) GameInfoHandler(w http.ResponseWriter, r *http.Request) {
 
 // GameWeaponInfoHandler retrieves information about the weapons in a game by its ID.
 func (ae *AppEnv) GameWeaponInfoHandler(w http.ResponseWriter, r *http.Request) {
-	// All responses will be JSON for now.
-	// acceptHeader := r.Header.Get("Accept")
-
 	gameID, err := strconv.Atoi(chi.URLParam(r, "id"))
 	if err != nil {
 		log.Printf("Invalid or missing game ID value: %s", err)
@@ -143,9 +140,39 @@ func (ae *AppEnv) GameWeaponInfoHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	type Series struct {
+		Key string `json:"key"`
+		Color string `json:"color"`
+		Values []*game.PlayerWeaponStatBase `json:"values"`
+	}
+
+	weaponToSeries := make(map[string]*Series)
+
+	// Assemble the weapon data in the "shape" that NVD3 wants. The series is the 
+	// distinct set of weapons, with each entry in the set being the list of weapon
+	// stats by the players. 
+	for _, ws := range gameWeaponInfo.WeaponStats {
+		series, ok := weaponToSeries[ws.WeaponCd]
+		if ok {
+			// Series already created, append
+			series.Values = append(series.Values, ws)
+		} else {
+			// Series not created yet. Create and add.
+			series := Series{Key: ws.WeaponCd, Values: make([]*game.PlayerWeaponStatBase, 0)}
+			series.Values = append(series.Values, ws)
+			weaponToSeries[ws.WeaponCd] = &series
+		}
+	}
+
+	// Flatten the map into a plain list of series
+	series := make([]*Series, 0)
+	for _, s := range weaponToSeries {
+		series = append(series, s)
+	}
+
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	bytes, err := json.Marshal(gameWeaponInfo)
+	bytes, err := json.Marshal(series)
 	if err != nil {
 		log.Printf("Could not marshal weapon info to JSON for game ID %d: %s", gameID, err)
 		http.Error(w, fmt.Sprintf("404 %s", http.StatusText(404)), 404)
