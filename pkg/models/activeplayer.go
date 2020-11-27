aa98268a3de3445f16607839268a17ab498aead0
Ant Zucaro <azucaro@gmail.com>
2020-11-27 13:38:08 -0500

    Add query for active players by map.

--- pkg/models/activeplayer.go
+++ pkg/models/activeplayer.go
@@ -64,3 +64,29 @@ func (ds *PGDatastore) RActivePlayersByServer(serverID int, cutoff *time.Time, l
 
 	return scanActivePlayers(rows)
 }
+
+// RActivePlayersByMap retrieves the active players for a given map.
+func (ds *PGDatastore) RActivePlayersByMap(mapID int, cutoff *time.Time, limit int) ([]*ActivePlayer, error) {
+	sql := `SELECT row_number() OVER (ORDER BY sum(player_game_stats.alivetime) DESC) AS rank, 
+	players.player_id AS players_player_id, players.nick AS players_nick, 
+	cast(extract(epoch from sum(player_game_stats.alivetime)) AS INTEGER) AS alivetime,
+	now() at time zone 'UTC' AS create_dt
+	FROM player_game_stats, players, games
+	WHERE players.player_id = player_game_stats.player_id 
+	AND games.game_id = player_game_stats.game_id 
+	AND games.map_id = $1
+	AND players.player_id > 2
+	AND player_game_stats.create_dt > $2
+	GROUP BY players.nick, players.player_id 
+	ORDER BY sum(player_game_stats.alivetime) DESC
+	LIMIT $3
+	`
+
+	rows, err := ds.db.Query(sql, mapID, cutoff, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return scanActivePlayers(rows)
+}
--- pkg/models/datastore.go
+++ pkg/models/datastore.go
@@ -59,6 +59,7 @@ type Datastore interface {
 	// Top Players by time played
 	RActivePlayers(limit, start int) ([]*ActivePlayer, error)
 	RActivePlayersByServer(serverID int, cutoff *time.Time, limit int) ([]*ActivePlayer, error)
+	RActivePlayersByMap(mapID int, cutoff *time.Time, limit int) ([]*ActivePlayer, error)
 
 	// Top Servers by aggregate play time
 	RActiveServers(limit, start int) ([]*ActiveServer, error)
