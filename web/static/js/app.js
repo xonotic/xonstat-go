ae4044d848814fc186ac81a3ecf431d7fdb32412
Ant Zucaro <azucaro@gmail.com>
2020-12-06 12:12:11 -0500

    Update script refs, move reltime to app.js.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation()
+
+// Relative time conversion for all pages.
+$('.abstime').each(function(i,e){
+    var epoch = e.getAttribute('data-epoch');
+    var d = new Date(0);
+    d.setUTCSeconds(epoch);
+    e.setAttribute('title', d.toDateString() + ' ' + d.toTimeString());  
+});
