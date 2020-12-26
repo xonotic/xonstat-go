5b36aad5efa4754877ef3aaa42a6321332ced574
Ant Zucaro <azucaro@gmail.com>
2020-12-26 08:42:50 -0500

    Show overall stats on the player page.

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,10 +1,15 @@
 package util
 
-// Percentage calculates the percentage of the given numbers.
-func Percentage(numerator, denominator int) float32 {
+// Ratio calculates the ratio between two numbers, handling divide-by-zero errors.
+func Ratio(numerator, denominator int) float32 {
 	if denominator == 0 {
 		denominator = 1
 	}
 
-	return float32(numerator) / float32(denominator) * 100.0
+	return float32(numerator) / float32(denominator)
+}
+
+// Percentage calculates the percentage of the given numbers.
+func Percentage(numerator, denominator int) float32 {
+	return Ratio(numerator, denominator) * 100.0
 }
