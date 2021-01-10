bc1fce251308d58e40c33eaf6a0c9ccb4d5af6c3
Ant Zucaro <azucaro@gmail.com>
2021-01-10 11:22:22 -0500

    Make skill calculation immutable, and operate on only one game at a time.

--- pkg/skill/wenglin.go
+++ pkg/skill/wenglin.go
@@ -1,19 +1,26 @@
 package skill
 
 import (
+	"fmt"
+	"log"
 	"math"
 	"time"
 )
 
 // Initial parameters for Ratings.
-const defaultMu = 25.0
-const defaultSigma = defaultMu/3
-const defaultBeta = defaultSigma/2
+// MU is the mean skill value for a brand new player.
+const MU = 25.0
+
+// SIGMA is the standard deviation for skill of a brand new player.
+const SIGMA = MU / 3
+
+// BETA is a component used in calculating Weng-Lin values.
+const BETA = SIGMA / 2
 
 // Rating is the Weng-Lin skill value for a given player. It is basically the two components
 // describing a normal distribution: mu (the mean) and sigma (the standard deviation).
 type Rating struct {
-	Mu float64
+	Mu    float64
 	Sigma float64
 }
 
@@ -25,6 +32,9 @@ type PlayerResult struct {
 	// How this player performed in the match. This figure is central to the skill
 	// calculations, thus it should be normalized across all participants in the.
 	Score float32
+
+	// TODO: K factor to scale skill deltas for those who haven't participated fully, played at
+	// a disadvantage, etc?
 }
 
 // MatchResult is the results of a match, and is used to calculate skill updates.
@@ -34,72 +44,68 @@ type MatchResult struct {
 
 	// The player performances that are to be evaluated.
 	PlayerResults []PlayerResult
-
-	// How long the match lasted.
-	Duration time.Duration
 }
 
-func addSkillIfMissing(pid int, skills map[int]*Rating) {
-	if _, ok := skills[pid]; !ok {
-		skills[pid] = &Rating{
-			Mu: defaultMu,
-			Sigma: defaultSigma,
-		}
-	}
-}
-
-// WengLinBT calculates the updates to the player skills using the Weng-Lin 
+// WengLinBT calculates the updates to the player skills using the Weng-Lin
 // Bradley-Terry full pair algorithm.
 // Original code here: http://www.csie.ntu.edu.tw/~cjlin/papers/online_ranking/
-func WengLinBT(matches []MatchResult, skills map[int]*Rating) {
-	for _, matchResult := range matches {
-		omega := make(map[int]float64, len(matchResult.PlayerResults))
-		delta := make(map[int]float64, len(matchResult.PlayerResults))
-		for _, p1 := range matchResult.PlayerResults {
-			omega[p1.PlayerID] = 0.0
-			delta[p1.PlayerID] = 0.0
-			for _, p2 := range matchResult.PlayerResults {
-				if p2.PlayerID == p1.PlayerID {
-					continue
-				}
+func WengLinBT(result MatchResult, skills []Rating) ([]Rating, error) {
+	// We expect the caller to provide the list of starting skills such that each
+	// player result in the match has a corresponding skill at the same index.
+	if len(result.PlayerResults) != len(skills) {
+		log.Printf("Mismatched data: %d players in game, %d skills provided.",
+			len(result.PlayerResults), len(skills))
 
-				// Ensure both players have skill values
-				addSkillIfMissing(p1.PlayerID, skills)
-				addSkillIfMissing(p2.PlayerID, skills)
+		return nil, fmt.Errorf("number of players and skills do not match")
+	}
 
-				p1SigmaSquared := math.Pow(skills[p1.PlayerID].Sigma, 2.0)
-				p2SigmaSquared := math.Pow(skills[p2.PlayerID].Sigma, 2.0)
-				betaSquared := math.Pow(defaultBeta, 2.0)
-				ciq := math.Sqrt(p1SigmaSquared + p2SigmaSquared + (2*betaSquared))
-
-				muDiff := skills[p2.PlayerID].Mu - skills[p1.PlayerID].Mu
-				piq := 1. / (1. + math.Exp(muDiff / ciq))
-
-				// TODO: This is currently winner-take-all. Implement scaling?
-				s := 0.0
-				if p1.Score > p2.Score {
-					// P1 won
-					s = 1
-				} else if p1.Score == p2.Score {
-					// P1 and P2 tied
-					s = 0.5
-				}
-
-				omega[p1.PlayerID] += (p1SigmaSquared / ciq) * (s - piq)
-                gamma := skills[p1.PlayerID].Sigma / ciq
-                delta[p1.PlayerID] += gamma * (p1SigmaSquared / ciq) / ciq * piq * (1 - piq)
+	omega := make(map[int]float64, len(result.PlayerResults))
+	delta := make(map[int]float64, len(result.PlayerResults))
+	for p1index, p1 := range result.PlayerResults {
+		omega[p1.PlayerID] = 0.0
+		delta[p1.PlayerID] = 0.0
+		for p2index, p2 := range result.PlayerResults {
+			if p2.PlayerID == p1.PlayerID {
+				continue
 			}
-		}
 
-		for _, player := range matchResult.PlayerResults {
-			// This is the resulting skill update after we've processed the match
-			skills[player.PlayerID].Mu += omega[player.PlayerID]
+			p1SigmaSquared := math.Pow(skills[p1index].Sigma, 2.0)
+			p2SigmaSquared := math.Pow(skills[p2index].Sigma, 2.0)
+			betaSquared := math.Pow(BETA, 2.0)
+			ciq := math.Sqrt(p1SigmaSquared + p2SigmaSquared + (2 * betaSquared))
 
-			d := 1-delta[player.PlayerID]
-			if d < 0.0001 {
-				d = 0.0001
+			muDiff := skills[p2index].Mu - skills[p1index].Mu
+			piq := 1. / (1. + math.Exp(muDiff/ciq))
+
+			// TODO: This is currently winner-take-all. Implement scaling?
+			s := 0.0
+			if p1.Score > p2.Score {
+				// P1 won
+				s = 1
+			} else if p1.Score == p2.Score {
+				// P1 and P2 tied
+				s = 0.5
 			}
-			skills[player.PlayerID].Sigma *= math.Sqrt(d)
+
+			omega[p1.PlayerID] += (p1SigmaSquared / ciq) * (s - piq)
+			gamma := skills[p1.PlayerID].Sigma / ciq
+			delta[p1.PlayerID] += gamma * (p1SigmaSquared / ciq) / ciq * piq * (1 - piq)
 		}
 	}
+
+	newSkills := make([]Rating, len(skills))
+	for i, player := range result.PlayerResults {
+		// Clamp the factor by which sigma changes.
+		d := 1 - delta[player.PlayerID]
+		if d < 0.0001 {
+			d = 0.0001
+		}
+
+		newSkills[i] = Rating{
+			Mu:    skills[i].Mu + omega[player.PlayerID],
+			Sigma: skills[i].Sigma * math.Sqrt(d),
+		}
+	}
+
+	return newSkills, nil
 }
