d250163e19d7d85ce69cd8244f61eb9ecc75a556
Ant Zucaro <azucaro@gmail.com>
2021-02-15 14:47:14 -0500

    Support JSON for game_info handler.

--- internal/handlers/game_info.go
+++ internal/handlers/game_info.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"gitlab.com/xonotic/xonstat/pkg/game"
@@ -29,12 +30,12 @@ type playerGameStatJSON struct {
 	Color            string `json:"color"`
 	AliveTime        string `json:"alivetime"`
 	AliveTimeMS      int    `json:"alivetime_ms"`
-	Kills            int    `json:"kill"`
+	Kills            int    `json:"kills"`
 	Deaths           int    `json:"deaths"`
 	Suicides         int    `json:"suicides"`
 	Score            int    `json:"score"`
 	Time             string `json:"time"`
-	TimeMS           string `json:"time_ms"`
+	TimeMS           int    `json:"time_ms"`
 	Captures         int    `json:"captures"`
 	Pickups          int    `json:"pickups"`
 	Drops            int    `json:"drops"`
@@ -63,22 +64,109 @@ type nonParticipantJSON struct {
 }
 
 type gameInfoJSONResponse struct {
-	GameID              int                        `json:"game_id"`
-	GameTypeCd          string                     `json:"game_type_cd"`
-	GameTypeDescr       string                     `json:"game_type_descr"`
-	Duration            string                     `json:"duration"`
-	Winner              int                        `json:"winner"`
-	MatchID             string                     `json:"match_id"`
-	Mod                 string                     `json:"mod"`
-	CreateDt            string                     `json:"create_dt"`
-	CreateDtFuzzy       string                     `json:"create_dt_fuzzy"`
-	ServerID            int                        `json:"server_id"`
-	MapID               int                        `json:"map_id"`
-	TeamGameStatsByTeam map[int]teamGameStatJSON   `json:"team_game_stats"`
-	PlayerGameStats     []playerGameStatJSON       `json:"player_game_stats"`
-	FragMatrix          map[int][]int              `json:"frag_matrix"`
-	Forfeits            []*game.NonParticipantBase `json:"forfeits"`
-	Spectators          []*game.NonParticipantBase `json:"spectators"`
+	GameID              int                      `json:"game_id"`
+	GameTypeCd          string                   `json:"game_type_cd"`
+	GameTypeDescr       string                   `json:"game_type_descr"`
+	Duration            string                   `json:"duration"`
+	DurationSecs        int                      `json:"duration_secs"`
+	Winner              int                      `json:"winner"`
+	MatchID             string                   `json:"match_id"`
+	Mod                 string                   `json:"mod"`
+	CreateDt            string                   `json:"create_dt"`
+	CreateDtFuzzy       string                   `json:"create_dt_fuzzy"`
+	ServerID            int                      `json:"server_id"`
+	MapID               int                      `json:"map_id"`
+	TeamGameStatsByTeam map[int]teamGameStatJSON `json:"team_game_stats"`
+	PlayerGameStats     []playerGameStatJSON     `json:"player_game_stats"`
+	Forfeits            []nonParticipantJSON     `json:"forfeits"`
+	Spectators          []nonParticipantJSON     `json:"spectators"`
+}
+
+func gameInfoBaseToJSON(ib game.InfoBase) ([]byte, error) {
+
+	teamGameStats := make(map[int]teamGameStatJSON, len(ib.TeamGameStatsByTeam))
+	for i, v := range ib.TeamGameStatsByTeam {
+		teamGameStats[i] = teamGameStatJSON{
+			Score:  v.Score,
+			Rounds: v.Rounds,
+			Caps:   v.Caps,
+			Color:  v.Color,
+		}
+	}
+
+	playerGameStats := make([]playerGameStatJSON, len(ib.PlayerGameStats))
+	for i, v := range ib.PlayerGameStats {
+		playerGameStats[i] = playerGameStatJSON{
+			PlayerGameStatID: v.PlayerGameStatID,
+			PlayerID:         v.PlayerID,
+			Nick:             v.Nick.NickStripped,
+			Team:             v.Team,
+			Color:            v.Color,
+			AliveTime:        v.AliveTime.Short,
+			AliveTimeMS:      int(v.AliveTime.Duration.Microseconds()),
+			Kills:            v.Kills,
+			Deaths:           v.Deaths,
+			Suicides:         v.Suicides,
+			Score:            v.Score,
+			Time:             v.Time.Short,
+			TimeMS:           int(v.Time.Duration.Milliseconds()),
+			Captures:         v.Captures,
+			Pickups:          v.Pickups,
+			Drops:            v.Drops,
+			Returns:          v.Returns,
+			Collects:         v.Collects,
+			Destroys:         v.Destroys,
+			Pushes:           v.Pushes,
+			CarrierFrags:     v.CarrierFrags,
+			Fastest:          v.Fastest.Short,
+			FastestMS:        int(v.Fastest.Duration.Milliseconds()),
+			AvgLatency:       v.AvgLatency,
+			ScoreboardPos:    v.ScoreboardPos,
+			Laps:             v.Laps,
+			Revivals:         v.Revivals,
+			Lives:            v.Lives,
+		}
+	}
+
+	toNonParticipantJSON := func(l []*game.NonParticipantBase) []nonParticipantJSON {
+		npJSON := make([]nonParticipantJSON, len(l))
+		for i, v := range l {
+			npJSON[i] = nonParticipantJSON{
+				PlayerGameNonParticipantID: v.PlayerGameNonParticipantID,
+				PlayerID:                   v.PlayerID,
+				GameID:                     v.GameID,
+				Nick:                       v.Nick.NickStripped,
+				AliveTime:                  v.AliveTime.Short,
+				AliveTimeMS:                int(v.AliveTime.Duration.Milliseconds()),
+				Score:                      int(v.Score),
+			}
+		}
+		return npJSON
+	}
+
+	forfeits := toNonParticipantJSON(ib.Forfeits)
+	spectators := toNonParticipantJSON(ib.Spectators)
+
+	response := gameInfoJSONResponse{
+		GameID:              ib.GameID,
+		GameTypeCd:          ib.GameTypeCd,
+		GameTypeDescr:       ib.GameTypeDescr,
+		Duration:            ib.Duration.Short,
+		DurationSecs:        int(ib.Duration.Duration.Seconds()),
+		Winner:              ib.Winner,
+		MatchID:             ib.MatchID,
+		Mod:                 ib.Mod,
+		CreateDt:            ib.CreateDt.Dt.Format(time.RFC3339),
+		CreateDtFuzzy:       ib.CreateDt.Fuzzy,
+		ServerID:            ib.ServerID,
+		MapID:               ib.MapID,
+		TeamGameStatsByTeam: teamGameStats,
+		PlayerGameStats:     playerGameStats,
+		Forfeits:            forfeits,
+		Spectators:          spectators,
+	}
+
+	return json.Marshal(response)
 }
 
 type gameInfoResponse struct {
@@ -133,9 +221,16 @@ func (ae *AppEnv) GameInfoHandler(w http.ResponseWriter, r *http.Request) {
 
 	if acceptHeader == "application/json" {
 		// JSON response
+		bytes, err := gameInfoBaseToJSON(*gameInfo)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			http.Error(w, fmt.Sprintf("500 %s", http.StatusText(500)), 500)
+			return
+		}
+
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		// w.Write()
+		w.Write(bytes)
 	} else {
 		// HTML response
 		serverInfo, err := server.InfoData(ae.db, gameInfo.ServerID)
