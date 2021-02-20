e35fa265676acab06b453c0df8f88b0c65535e93
Ant Zucaro <azucaro@gmail.com>
2021-02-20 08:36:40 -0500

    Refactor the player_recent_games_fragment handler.

---
+++ internal/handlers/player_recent_games_fragment.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/spf13/viper"
+	"gitlab.com/xonotic/xonstat/pkg/game"
+)
+
+// PlayerRecentGamesFragmentHandler retrieves just the recent games table.
+func (ae *AppEnv) PlayerRecentGamesFragmentHandler(w http.ResponseWriter, r *http.Request) {
+	playerID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("invalid or missing player ID value: %s", err)
+		ae.NotFoundHandler(w, r)
+		return
+	}
+
+	params := r.URL.Query()
+	gameTypeCd := params.Get("game_type_cd")
+
+	// The "overall" game type means no filter.
+	if gameTypeCd == "overall" {
+		gameTypeCd = ""
+	}
+
+	recentGamesCutoff := time.Now().UTC().AddDate(0, 0, -1*viper.GetInt("RecentGamesDays"))
+	recentGames, _ := game.RecentGamesData(ae.db, game.EmptyServerID, game.EmptyMapID, playerID,
+		gameTypeCd, &recentGamesCutoff, game.EmptyStartGameID, game.EmptyEndGameID, 20)
+
+	err = ae.templates["recentgames.fragment.page.html"].Execute(w, recentGames)
+	if err != nil {
+		log.Printf("Error: %s", err)
+		http.Error(w, fmt.Sprintf("500 %s", http.StatusText(500)), 500)
+		return
+	}
+}
