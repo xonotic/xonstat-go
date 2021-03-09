0ec659e15e528cd2a592b9f22f7da38b0234c222
Ant Zucaro <azucaro@gmail.com>
2021-03-09 17:07:39 -0500

    Add cancellation and better query structure for RMapActivePlayerScores.

--- pkg/models/activeplayerscore.go
+++ pkg/models/activeplayerscore.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"context"
 	"database/sql"
 	"time"
 )
@@ -54,21 +55,29 @@ func (ds *PGDatastore) RServerActivePlayerScores(serverID int, cutoff *time.Time
 
 // RMapActivePlayerScores retrieves the top scoring players for a map over a given period of time.
 func (ds *PGDatastore) RMapActivePlayerScores(mapID int, cutoff *time.Time, limit int) ([]*ActivePlayerScore, error) {
-	sql := `SELECT row_number() OVER (ORDER BY sum(player_game_stats.score) DESC) AS rank, 
-	players.player_id AS players_player_id, players.nick AS players_nick, 
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	sql := `SELECT
+	row_number() OVER (ORDER BY sum(player_game_stats.score) DESC) AS rank,
+	players.player_id AS players_player_id, players.nick AS players_nick,
 	sum(player_game_stats.score) AS total_score
-	FROM player_game_stats, players, games
-	WHERE players.player_id = player_game_stats.player_id 
-	AND games.game_id = player_game_stats.game_id 
-	AND games.map_id = $1
-	AND players.player_id > 2 
-	AND player_game_stats.create_dt > $2
-	AND games.create_dt > $2
-	GROUP BY players.nick, players.player_id 
-	ORDER BY sum(player_game_stats.score) DESC
+
+	FROM player_game_stats
+	INNER JOIN players USING (player_id)
+	INNER JOIN games USING (game_id)
+
+	WHERE games.map_id = $1
+	AND players.player_id > 2
+	AND player_game_stats.create_dt BETWEEN $2 AND (now() at time zone 'UTC' + interval '1 day')
+	AND games.create_dt BETWEEN $2 AND (now() at time zone 'UTC' + interval '1 day')
+
+	GROUP BY players_nick, players_player_id
+	ORDER BY total_score DESC
+
 	LIMIT $3`
 
-	rows, err := ds.db.Query(sql, mapID, cutoff, limit)
+	rows, err := ds.db.QueryContext(ctx, sql, mapID, cutoff, limit)
 	if err != nil {
 		return nil, err
 	}
