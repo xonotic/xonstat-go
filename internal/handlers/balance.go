0d7259415b84945e187e720a3490f3787a1f7488
Ant Zucaro <azucaro@gmail.com>
2022-09-10 11:49:12 -0400

    Per-match random seeds, default skill handling, score factoring.

--- internal/handlers/balance.go
+++ internal/handlers/balance.go
@@ -3,10 +3,14 @@ package handlers
 import (
 	"bufio"
 	"fmt"
+	"hash/fnv"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 	"strings"
 
+	wenglin "gitlab.com/xonotic/xonstat/pkg/skill"
 	"gitlab.com/xonotic/xonstat/pkg/submission"
 )
 
@@ -25,34 +29,43 @@ type balanceResponse struct {
 	Players []*balancePlayer
 }
 
-// BalanceHandler godoc
-// @Summary Best guess ordering of players according to skill and score data.
-// @Accept  text/plain
-// @Produce  text/plain
-// @Param jitter query int false "random swaps to do after sorting"
-// @Success 200 {object} balanceResponse
-// @Router /balance [post]
-func (ae *AppEnv) BalanceHandler(w http.ResponseWriter, r *http.Request) {
+// preprocess handles assembling the POST body into a type that we can work with
+func preprocess(w http.ResponseWriter, r *http.Request) (*submission.Submission, error) {
 	bodyReader := bufio.NewReader(r.Body)
 
 	rawSubmission, err := submission.NewRawSubmission(bodyReader)
 	if err != nil {
 		log.Printf("Error: %s", err)
 		http.Error(w, fmt.Sprintf("422 %s", http.StatusText(422)), 422)
-		return
+		return nil, err
 	}
 
 	// It doesn't make sense to balance games with only one or two players.
 	if rawSubmission.NumHumansPlayed < 3 {
 		log.Printf("Error: not enough players to balance")
 		http.Error(w, fmt.Sprintf("422 %s", http.StatusText(422)), 422)
-		return
+		return nil, fmt.Errorf("not enough players to balance")
 	}
 
 	sub, err := submission.NewSubmission(rawSubmission)
 	if err != nil {
 		log.Printf("Error: %s", err)
 		http.Error(w, fmt.Sprintf("422 %s", http.StatusText(422)), 422)
+		return nil, err
+	}
+
+	return sub, nil
+}
+
+// BalanceHandler godoc
+// @Summary Best guess ordering of players according to skill and score data.
+// @Accept  text/plain
+// @Produce  text/plain
+// @Success 200 {object} balanceResponse
+// @Router /balance [post]
+func (ae *AppEnv) BalanceHandler(w http.ResponseWriter, r *http.Request) {
+	sub, err := preprocess(w, r)
+	if err != nil {
 		return
 	}
 
@@ -62,8 +75,32 @@ func (ae *AppEnv) BalanceHandler(w http.ResponseWriter, r *http.Request) {
 	// Use hashkeys to make updates to entries in the above slice.
 	hashkeysToPlayers := make(map[string]*balancePlayer, 0)
 
+	// Per-match random seeds (hashkey + match ID)
+	hashkeysToSeeds := make(map[string]int64, 0)
+
+	// Players that actually have skills/ratings in the DB.
+	hashkeysWithSkills := make(map[string]struct{})
+
 	// Hashkeys for tracked players
 	tracked := make([]string, 0)
+
+	// Keep track of the maxes, for use in scaling later.
+	var maxScorePerSecond float64 = 1.0
+	var maxSkill float64 = 1.0
+
+	// In case there are NO skill values to establish a "match average",
+	// we will use the default ones.
+	avgMu := wenglin.DefaultParams.DefaultMu
+	avgSigma := wenglin.DefaultParams.DefaultSigma
+
+	// TODO: take this as input
+	randomness := 1.0
+
+	// TODO: establish a better value
+	scoreFactor := 0.25
+
+	// FNV1A64
+	hash := fnv.New64a()
 
 	for hashkey, player := range sub.PlayersByHashkey {
 		gamestat, hasGameStat := sub.PlayerGameStatsByHashkey[hashkey]
@@ -72,9 +109,19 @@ func (ae *AppEnv) BalanceHandler(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 
+		// Use a consistent per-match seed
+		hash.Reset()
+		hash.Write([]byte(hashkey + sub.Game.MatchID.String))
+		seed := int64(hash.Sum64())
+		hashkeysToSeeds[hashkey] = seed
+
 		score := gamestat.Score.Int32
 		alivetimesecs := sub.PlayerGameStatsByHashkey[hashkey].AliveTime.Seconds()
 		sps := float64(score) / alivetimesecs
+
+		if sps > maxScorePerSecond {
+			maxScorePerSecond = sps
+		}
 
 		player := balancePlayer{
 			Hashkey:        hashkey,
@@ -88,14 +135,14 @@ func (ae *AppEnv) BalanceHandler(w http.ResponseWriter, r *http.Request) {
 		hashkeysToPlayers[hashkey] = &player
 
 		if !strings.HasPrefix(hashkey, "player#") {
-			// This is a tracked player, and might have a skill record.
-			// 1. Save these types of hashkeys
+			// This is a tracked player, and might have a skill record. Save these to query the DB.
 			tracked = append(tracked, hashkey)
 		}
 	}
 
+	// Those that are tracked are the ones that might have skill values in the DB.
+	// We query for them in batch to decrease round-trips. 
 	if len(tracked) > 0 {
-		// 2. Query for skills for those hashkeys
 		skills, err := ae.db.RPlayerSkillsBatch(tracked, sub.Game.GameTypeCd)
 		if err != nil {
 			log.Printf("Error: %s", err)
@@ -103,13 +150,43 @@ func (ae *AppEnv) BalanceHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		// 3. Update the corresponding balancePlayer record w/ skill info (if found)
+		var sumMu, sumSigma float64
+
 		for _, skill := range skills {
-			// Optimistic (right end of the confidence interval): we are ~97% confident that
-			// this player's skill is LESS than this number.
+			hashkeysWithSkills[skill.Hashkey] = struct{}{}
+
+			sumMu += skill.Mu
+			sumSigma += skill.Sigma
+
 			player := hashkeysToPlayers[skill.Hashkey]
-			player.Skill = skill.Mu + (3 * skill.Sigma)
+
+			rand.Seed(hashkeysToSeeds[skill.Hashkey])
+
+			player.Skill = math.Exp((rand.NormFloat64()*(skill.Sigma*randomness)+skill.Mu)/wenglin.DefaultParams.DefaultBeta)
+			if player.Skill > maxSkill {
+				maxSkill = player.Skill
+			}
 		}
+
+		if len(skills) > 1 {
+			// We DO have at least two data points to establish an average skill.
+			avgMu = sumMu / float64(len(skills))
+			avgSigma = sumSigma / float64(len(skills))
+		}
+	}
+
+	// Final pass to factor in the score
+	scorePerSecondScale := maxSkill / float64(maxScorePerSecond)
+	for hashkey, player := range hashkeysToPlayers {
+		if _, ok := hashkeysWithSkills[hashkey]; !ok {
+			// This player doesn't yet have a skill. Derive one from the default.
+			// Use a consistent per-match seed
+			rand.Seed(hashkeysToSeeds[hashkey])
+			player.Skill = math.Exp((rand.NormFloat64()*(avgSigma*randomness)+avgMu)/wenglin.DefaultParams.DefaultBeta)
+		}
+
+		// Finally, add score to the linearized value
+		player.Skill += scorePerSecondScale * player.ScorePerSecond * scoreFactor
 	}
 
 	response := balanceResponse{
