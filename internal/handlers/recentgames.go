f0aa3f01ce70c1198dcbbadf6c7c0dbc2f770ef0
Ant Zucaro <azucaro@gmail.com>
2023-04-08 16:54:56 -0400

    Show t/mayhem selectors on the recent games page.

--- internal/handlers/recentgames.go
+++ internal/handlers/recentgames.go
@@ -131,7 +131,7 @@ func (ae *AppEnv) RecentGamesHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write(recentGames)
 	} else {
 		// HTML response
-		gameTypeCds := []string{"overall", "duel", "ctf", "dm", "tdm", "ca", "kh", "ft", "as", "dom", "nb", "cts"}
+		gameTypeCds := []string{"overall", "duel", "ctf", "dm", "tdm", "ca", "kh", "ft", "as", "dom", "nb", "cts", "mayhem", "tmayhem"}
 
 		// Set the query string for pagination
 		if len(recentGames) > 0 {
